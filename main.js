f0ddac279e512d6124e8be1133309ab096f54a9e
Presse Matthieu <matthieu.presse@ouest-france.fr>
2018-08-31 14:51:35 +0200

    [TECH] reindent only

--- main.js
+++ main.js
@@ -1,95 +1,95 @@
 $(function(){
-  //On input keyup
-  $('body').on('keyup', '.b-cat input', function(){
-    var $this = $(this),
-        val = parseInt($this.val());
+    //On input keyup
+    $('body').on('keyup', '.b-cat input', function(){
+        var $this = $(this),
+                val = parseInt($this.val());
 
-    //Set widths of categories
-    setCatWidths($this.parents('.b'));
+        //Set widths of categories
+        setCatWidths($this.parents('.b'));
 
-    calculateActualSize($(this).parents('.b'));
-  });
-
-  //Set width of category
-  function setCatWidths(scope) {
-    scope.find('.b-cat input').each(function(){
-       var $this = $(this),
-           val = parseInt($this.val());
-      $this.parent().width(calculateCatWidth(scope,val)+'%');
-    });
-  }
-
-  $('.b').each(function(){
-    setCatWidths($(this));
-  })
-
-  //Adjust budget value
-  $('body').on('keyup', '.b-size span', function(){
-    //Check if actual size is over/under budget
-    budgetOverUnder($(this).parents('.b'));
-  });
-
-  //Calculate Actual Template Size
-  function calculateActualSize(scope) {
-    var inputs = scope.find('.b-cat input'),
-        numInputs = inputs.length,
-        totalActualSize = 0;
-
-    //Calculate total value from each category
-    inputs.each(function(){
-       var val = parseInt($(this).val());
-       totalActualSize = totalActualSize + val;
-
-       $(this).val('' + val/10 + '%');
+        calculateActualSize($(this).parents('.b'));
     });
 
-    setActualSize(scope,totalActualSize);
-
-    //Check if actual size is over/under budget
-    budgetOverUnder(scope);
-  }
-
-  $('.b').each(function(){
-    calculateActualSize($(this));
-  })
-
-  //Calculate width
-  function calculateCatWidth(scope,val) {
-    var actualSize = parseInt(scope.find('.actual-size span').text());
-    console.log(actualSize);
-    var categoryWidth = (val/actualSize)*100;
-    return categoryWidth;
-  }
-
-  //Set Actual Size
-  function setActualSize(scope,val) {
-    actualSize = val;
-    scope.find('.actual-size span').text(val);
-  }
-
-  //Determine if actual size is within budget
-  function budgetOverUnder(scope) {
-    var budgetSize = parseInt(scope.find('.b-size span').text());
-    var actualSize = parseInt(scope.find('.actual-size span').text());
-    var totalSize = actualSize/budgetSize;
-
-    //If actual val is over budget
-    if(totalSize>1) {
-      scope.find('.b-header, .actual-size').addClass('has-error');
-     scope.find('.b-cat-container').width('100%');
-    } else {
-      scope.find('.b-header, .actual-size').removeClass('has-error');
-      scope.find('.b-cat-container').width(totalSize*100+'%');
+    //Set width of category
+    function setCatWidths(scope) {
+        scope.find('.b-cat input').each(function(){
+             var $this = $(this),
+                     val = parseInt($this.val());
+            $this.parent().width(calculateCatWidth(scope,val)+'%');
+        });
     }
-  }
 
-  $('.b').each(function(){
-    budgetOverUnder($(this));
-  })
+    $('.b').each(function(){
+        setCatWidths($(this));
+    })
 
-  //Make new Budget
-  $('.btn').on('click', function(){
-    $('.b').last().clone().insertBefore('.btn');
-  });
+    //Adjust budget value
+    $('body').on('keyup', '.b-size span', function(){
+        //Check if actual size is over/under budget
+        budgetOverUnder($(this).parents('.b'));
+    });
+
+    //Calculate Actual Template Size
+    function calculateActualSize(scope) {
+        var inputs = scope.find('.b-cat input'),
+                numInputs = inputs.length,
+                totalActualSize = 0;
+
+        //Calculate total value from each category
+        inputs.each(function(){
+             var val = parseInt($(this).val());
+             totalActualSize = totalActualSize + val;
+
+             $(this).val('' + val/10 + '%');
+        });
+
+        setActualSize(scope,totalActualSize);
+
+        //Check if actual size is over/under budget
+        budgetOverUnder(scope);
+    }
+
+    $('.b').each(function(){
+        calculateActualSize($(this));
+    })
+
+    //Calculate width
+    function calculateCatWidth(scope,val) {
+        var actualSize = parseInt(scope.find('.actual-size span').text());
+        console.log(actualSize);
+        var categoryWidth = (val/actualSize)*100;
+        return categoryWidth;
+    }
+
+    //Set Actual Size
+    function setActualSize(scope,val) {
+        actualSize = val;
+        scope.find('.actual-size span').text(val);
+    }
+
+    //Determine if actual size is within budget
+    function budgetOverUnder(scope) {
+        var budgetSize = parseInt(scope.find('.b-size span').text());
+        var actualSize = parseInt(scope.find('.actual-size span').text());
+        var totalSize = actualSize/budgetSize;
+
+        //If actual val is over budget
+        if(totalSize>1) {
+            scope.find('.b-header, .actual-size').addClass('has-error');
+         scope.find('.b-cat-container').width('100%');
+        } else {
+            scope.find('.b-header, .actual-size').removeClass('has-error');
+            scope.find('.b-cat-container').width(totalSize*100+'%');
+        }
+    }
+
+    $('.b').each(function(){
+        budgetOverUnder($(this));
+    })
+
+    //Make new Budget
+    $('.btn').on('click', function(){
+        $('.b').last().clone().insertBefore('.btn');
+    });
 
 });
