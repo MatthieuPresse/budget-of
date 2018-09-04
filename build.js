21a04ffa07d9ff58fb5c4ba55785811ce9a80e34
MatthieuPresse <matthieu.presse@gmail.com>
2018-09-04 22:51:19 +0200

    clean console

--- build.js
+++ build.js
@@ -1,8 +1,6 @@
 var request = require("request");
 var fs = require('fs');
 
-console.log(process.env);
-
 [{id: 8461, file: 'home.har'}, {id: 9821, file: 'detail-article.har'}].map(function(el){
 request({
     url: 'https://www.dareboost.com/api/0.5/monitoring/last-report',
@@ -13,7 +11,6 @@ request({
     }
 }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-        console.log(body)
         request(body.report.harFileUrl).pipe(fs.createWriteStream(el.file));
     } else {
         console.log("Err: ", error);
