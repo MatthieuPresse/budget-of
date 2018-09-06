92eea8dfe8818a7c295bb17ecc7ab6f7c653bac8
Presse Matthieu <matthieu.presse@ouest-france.fr>
2018-09-06 10:25:31 +0200

    [TECH] exit on build error

--- build.js
+++ build.js
@@ -2,18 +2,19 @@ var request = require("request");
 var fs = require('fs');
 
 [{id: 8461, file: 'home.har'}, {id: 9821, file: 'detail-article.har'}].map(function(el){
-request({
-    url: 'https://www.dareboost.com/api/0.5/monitoring/last-report',
-    method: "POST",
-    json: {
-        "token": process.env.dareboostApiKey,
-        "monitoringId": el.id
-    }
-}, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        request(body.report.harFileUrl).pipe(fs.createWriteStream(el.file));
-    } else {
-        console.log("Err: ", error);
-    }
-});
+    request({
+        url: 'https://www.dareboost.com/api/0.5/monitoring/last-report',
+        method: "POST",
+        json: {
+            "token": process.env.dareboostApiKey,
+            "monitoringId": el.id
+        }
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            request(body.report.harFileUrl).pipe(fs.createWriteStream(el.file));
+        } else {
+            console.log("Err: ", error);
+            process.exit(1);
+        }
+    });
 });
