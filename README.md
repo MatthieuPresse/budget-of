2eb6636da74d234597fabf4318b3a77f93f5cf98
Presse Matthieu <matthieu.presse@ouest-france.fr>
2018-10-03 17:44:10 +0200

    [TECH] refacto multi site

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # budget-of
+
+configuration en variable d environnement
+export configBuild='{"monitoring":[{"id":XXX,"file":"page.har","type_page":"PAGE_XXX"}]}'
+export dareboostApiKey=XXX
+
+script à lancer pour regenerer la page
+```
+npm run build
+```
--- build.js
+++ build.js
@@ -1,20 +1,26 @@
 var request = require("request");
 var fs = require('fs');
+var config = JSON.parse(process.env.configBuild);
 
-[{id: 8461, file: 'home.har'}, {id: 9821, file: 'detail-article.har'}].map(function(el){
-    request({
-        url: 'https://www.dareboost.com/api/0.5/monitoring/last-report',
-        method: "POST",
-        json: {
-            "token": process.env.dareboostApiKey,
-            "monitoringId": el.id
-        }
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            request(body.report.harFileUrl).pipe(fs.createWriteStream(el.file));
-        } else {
-            console.log("Err: ", error);
-            process.exit(1);
-        }
+console.log('ApiKey: ', process.env.dareboostApiKey);
+console.log('Configuration: ', config);
+
+fs.writeFile('./config.js', 'window.configBuild = ' + process.env.configBuild, 'utf8', function(){
+    (config.monitoring).map(function(el){
+        request({
+            url: 'https://www.dareboost.com/api/0.5/monitoring/last-report',
+            method: "POST",
+            json: {
+                "token": process.env.dareboostApiKey,
+                "monitoringId": el.id
+            }
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                request(body.report.harFileUrl).pipe(fs.createWriteStream(el.file));
+            } else {
+                console.log("Err: ", error);
+                process.exit(1);
+            }
+        });
     });
 });
